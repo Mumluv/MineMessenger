08eb036c99948ef3051e182a87a9081fe0850cef
Mumluv <merenkov1910@list.ru>
2024-04-24 21:58:51 +0300

    Изменения 2

---
+++ MineMessenger/AnotherAcc.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MineMessenger
+{
+    /// <summary>
+    /// Логика взаимодействия для AnotherAcc.xaml
+    /// </summary>
+    public partial class AnotherAcc : Window
+    {
+        public AnotherAcc()
+        {
+            InitializeComponent();
+        }
+    }
+}
